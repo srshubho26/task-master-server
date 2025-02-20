43f6e3de1a5bcceda501b3d4965e863d48ed9f65
S.R. Shuvo <shuvo22890@gmail.com>
2025-02-20 12:08:50 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynkon.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// Middleware
+app.use(cors({
+    origin: [
+        "http://localhost:5173"
+    ],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized' })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+async function run() {
+    try {
+        const database = client.db("task_master");
+        const usersCollection = database.collection("users");
+
+        // Create JWT after sign in
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+
+            const isUserExist = await usersCollection.findOne({email: user.email});
+            if(!isUserExist){
+                await usersCollection.insertOne(user);
+            }
+            
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '5h'});
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            }).send({ success: true })
+        })
+
+        // Clear cookie after logging out
+        app.post('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            }).send({ success: true })
+        })
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`)
+})
